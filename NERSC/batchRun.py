aabc7d3dd3908e64ad945a8488dbe55a9341c2c2
adamm <amwe@ucdavis.edu>
2024-04-12 18:17:39 -0700

    Updated evol batching scripts for NERSC compatibility. Pending testing

---
+++ NERSC/batchRun.py
@@ -0,0 +1,216 @@
+''' 
+This file is a *highly* modified version of the file batchRun.py 
+from the NetPyNE tutorial 9. 
+
+Evolutionary algorithm optimization of a network using NetPyNE
+To run use: mpiexec -np [num_cores] nrniv -mpi batchRun.py
+Note: May or may not include capability for other options in here later.
+'''
+
+'''
+Initialize
+'''
+##General Imports
+import os
+import shutil
+import json
+import pickle
+import glob
+import sys
+import json
+import os
+import pickle
+
+## NetPyne Imports
+#sys.path.insert(0, '/mnt/disk15tb/adam/git_workspace/netpyne_2DNetworkSimulations/netpyne')
+from netpyne import specs
+from netpyne.batch import Batch
+
+## Fitness Function Imports
+from fitness_functions import *
+from fitness_config import *
+
+## Logger
+import logging
+#set up logging
+script_dir = os.path.dirname(os.path.realpath(__file__))
+log_file = f'{script_dir}/batchRun.log'
+logging.basicConfig(filename=log_file, level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+# Define a logger
+logger = logging.getLogger(__name__)
+
+## Get the batch configuration
+def get_batch_config(batch_config_options = None):
+
+    ## Validate batch_config_options    
+    assert not batch_config_options is None, "batch_config_options must be defined"
+    assert 'method' in batch_config_options, 'method must be specified in batch_config_options'
+    # assert 'core_num' in batch_config_options, 'core_num must be specified in batch_config_options'
+    # assert 'nodes' in batch_config_options, 'nodes must be specified in batch_config_options'
+    # assert 'pop_per_core' in batch_config_options, 'pop_per_core must be specified in batch_config_options'
+    # assert 'duration_seconds' in batch_config_options, 'duration_seconds must be specified in batch_config_options'
+    assert 'pop_size' in batch_config_options, 'pop_size must be specified in batch_config_options'
+    assert 'max_generations' in batch_config_options, 'max_generations must be specified in batch_config_options'
+    assert 'num_elites' in batch_config_options, 'num_elites must be specified in batch_config_options'
+    # assert 'time_sleep' in batch_config_options, 'time_sleep must be specified in batch_config_options'
+    # assert 'maxiter_wait' in batch_config_options, 'maxiter_wait must be specified in batch_config_options'
+    # assert 'skip' in batch_config_options, 'skip must be specified in batch_config_options'
+    assert 'cfgFile' in batch_config_options, 'cfgFile must be specified in batch_config_options'
+    assert 'netParamsFile' in batch_config_options, 'netParamsFile must be specified in batch_config_options'
+    
+    # Extract the parameters
+    pop_size = batch_config_options['pop_size']
+    max_generations = batch_config_options['max_generations']
+    method = batch_config_options['method']
+    run_path =  batch_config_options['run_path']
+    core_num = batch_config_options['core_num']
+    nodes = batch_config_options['nodes']
+    batch_label = batch_config_options['batchLabel']
+    time_sleep = batch_config_options['time_sleep']
+    maxiter_wait = batch_config_options['maxiter_wait']
+    skip = batch_config_options['skip']
+    num_elites = batch_config_options['num_elites']
+    script = batch_config_options['script']
+    cfgFile = batch_config_options['cfgFile']
+    netParamsFile = batch_config_options['netParamsFile']
+    initCfg = None
+    if 'initCfg' in batch_config_options:
+        initCfg = batch_config_options['initCfg']
+
+    assert isinstance(pop_size, int), 'pop_size must be an integer'
+    assert isinstance(max_generations, int), 'max_generations must be an integer'
+    assert method in ['evol', 'grid'], 'method must be either "evol" or "grid"'
+
+    # Save the dictionary as a JSON file
+    #method = 'evol'
+
+    batch_config = {
+        'batchLabel': batch_label,
+        'saveFolder': run_path,
+        'method': method,
+        'cfgFile': cfgFile,
+        'netParamsFile': netParamsFile,
+        'runCfg': {
+            'type': 'mpi_bulletin',
+            'script': script,
+            'mpiCommand': 'mpirun',
+            'nodes': nodes,
+            'coresPerNode': core_num,
+            'allocation': 'default',
+            'reservation': None,
+            'skip': skip,
+        },
+        'evolCfg': {
+            'evolAlgorithm': 'custom',
+            'fitnessFunc': fitnessFunc, # fitness expression (should read simData)
+            'fitnessFuncArgs': {**fitnessFuncArgs, 'pop_size': pop_size},
+            'pop_size': pop_size,
+            'num_elites': num_elites,
+            'mutation_rate': 0.4,
+            'crossover': 0.5,
+            'maximize': False,
+            'max_generations': max_generations,
+            'time_sleep': time_sleep, # wait this time before checking again if sim is completed (for each generation)
+            'maxiter_wait': maxiter_wait, # max number of times to check if sim is completed (for each generation)
+            #effectively 1.5 hours per gen, max
+            'defaultFitness': 1000,
+            'initCfg': initCfg,
+        }
+    }  
+    
+    # # Save the dictionary as a pickle file
+    # with open('batch_config.pickle', 'wb') as f:
+    #     pickle.dump(batch_config, f)
+
+    return batch_config
+
+
+## Function to serialize the batch_config dictionary
+class batchcfgEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if callable(obj):
+            return str(obj)
+        return super().default(obj)
+
+def batchRun(batch_config = None):
+    
+    #Get batch_config
+    assert batch_config is not None, 'batch_config must be specified'
+
+    #load param_space from batch_run_path json
+    batch_run_path = batch_config['saveFolder']
+    batch_method = batch_config['method']
+    if batch_method == 'evol':
+        #define population params held constant across batch of simulations
+        from evol_param_config import evol_param_space
+        params = evol_param_space
+        # define_population_params(batch_run_path = batch_run_path)
+    elif batch_method == 'grid':
+        logger.error('Grid method not yet implemented')
+    else:
+        logger.error('Invalid method specified in batch_config')
+
+	# create Batch object with paramaters to modify, and specifying files to use
+    batch = Batch(params=params)
+    # Iterate over items in batch_config
+    for key, value in batch_config.items():
+        # If batch has an attribute with the same name as the key
+        if hasattr(batch, key):
+            # Set the attribute to the value from batch_config
+            setattr(batch, key, value)
+
+    ## Prepare method specific parameters
+    batch.runCfg = batch_config['runCfg']
+    if 'evolCfg' in batch_config: batch.evolCfg = batch_config['evolCfg']    
+
+    #run batch
+    batch.run()
+
+def init_batch_cfg():
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    logging.info(f'Batch script path: {script_dir}')
+
+    ## Output path
+    output_path = f'{script_dir}/output/' 
+    # Check if the output directory exists, if not, create it
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    logging.info(f'Output path: {output_path}')
+
+    ## Load batch_config_options.json    
+    logging.info(f'Batch config options: {batch_config_options}')
+    batch_run_path = batch_config_options['run_path']
+    # Save batch_config in run_path as JSON    
+    with open(f'{batch_run_path}/batch_config_options.json', 'w') as f:
+        json.dump(batch_config_options, f, cls = batchcfgEncoder)
+    logging.info(f'Batch config options saved in {batch_run_path}/batch_config_options.json')
+
+    #Generate batch config from batch_config_options
+    batch_config = get_batch_config(batch_config_options = batch_config_options)
+    logging.info(f'Batch config: {batch_config}')
+    #extract_batch params
+    batch_run_path =  batch_config['saveFolder']
+    # Save batch_config in run_path as JSON    
+    with open(f'{batch_run_path}/batch_config.json', 'w') as f:
+        json.dump(batch_config, f, cls = batchcfgEncoder)
+    logging.info(f'Batch config saved in {batch_run_path}/batch_config.json')
+
+    #Validate batch_config before running
+    assert 'method' in batch_config, 'method must be specified in batch_config'
+    assert 'skip' in batch_config['runCfg'], 'skip must be specified in batch_config'
+    assert 'batchLabel' in batch_config, 'batchLabel must be specified in batch_config'
+
+    return batch_config
+
+# Main code
+if __name__ == '__main__':
+    ## initialize batch_run_cfg
+    logging.info(f'Initializing batch config')
+    from batch_run_config import *
+    batch_config = init_batch_cfg()
+
+    #run batch
+    logging.info(f'Running batch: {batch_config["batchLabel"]}')
+    batchRun(batch_config = batch_config)
+    logging.info(f'Batch run completed') 
---
+++ NERSC/batch_run_config.py
@@ -0,0 +1,134 @@
+import os
+import pickle
+import json
+import shutil
+import sys
+import json
+
+## Logger
+import logging
+#set up logging
+script_dir = os.path.dirname(os.path.realpath(__file__))
+log_file = f'{script_dir}/batchRun.log'
+logging.basicConfig(filename=log_file, level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+# Define a logger
+logger = logging.getLogger(__name__)
+
+'''
+USER INPUT
+'''
+## Edit the following parameters as needed ##
+## Edit params in the batch_run_files folder as needed (with care) ##
+
+## Edit Run_Name to a unique name for the batch run ##
+try: run_name = sys.argv[1] #batch_run_files folder name
+except: run_name = 'NERSC_Test' ### Change this to a unique name for the batch run
+
+## Batch Params ##
+method = 'evol' #evolutionary algorithm
+# method = 'grid' #grid search
+
+## Selecte a candidate configuration to start the evolution from ##
+selected_cand_cfg = None
+# selected_cand_cfg = '/mnt/disk15tb/adam/git_workspace/netpyne_2DNetworkSimulations/2DNet_simulations/BurstingPlotDevelopment/nb5.1_optimizing_EEonly/output/24-3-24_5sec_EEsearch/gen_5/gen_5_cand_29_cfg.json'
+
+## Parallelization Parameters ##
+pop_per_core = 1
+core_num = 4
+num_nodes = 1
+pop_size = pop_per_core * core_num
+num_elite_percent = 10/100 # top 10% of the population will be copied to the next generation, this is considered high-medium elitism
+num_elites = int(num_elite_percent * pop_size)
+#duration_seconds = 5
+
+## Overwrite Parameters ##
+overwrite_run = True #True will overwrite any existing run with the same name
+continue_run = False #True will continue most recent run
+skip = True #True will skip the simulation if it already exists
+#make sure batch_run_path does not exist. Avoid overwriting data from any Run
+#overwrite_run = True #comment out later, this is for debugging
+
+'''
+Initialize
+'''
+##Prepare Batch_Run_Folder and Initial Files
+#output_path = nb_path.replace('batch_run_files', 'output')'
+script_path = os.path.dirname(os.path.realpath(__file__))
+output_path = script_path+'/output'
+#get unique run path
+i = 0
+first_run = True
+unique_run_path = f'{output_path}/run{i}_{run_name}'
+while os.path.exists(unique_run_path):
+    first_run = False
+    i += 1
+    unique_run_path = f'{output_path}/run{i}_{run_name}'
+
+## Mediate Overwrite
+if first_run is False:
+    assert not (overwrite_run and continue_run), 'overwrite_run and continue_run cannot both be True'
+    #Manage batch_run path
+    if overwrite_run or continue_run:
+        i = i-1
+        run_path = f'{output_path}/run{i}_{run_name}'
+    else:
+        run_path = unique_run_path    
+    #if continue-run is True, skip must be True
+    if continue_run is True:
+        assert skip is True, 'skip must be True if continue_run is True'
+        logger.info(f'Continuing most recent batch_run: {os.path.basename(run_path)}')    
+    #if overwrite-run is True, delete the existing run_path
+    if overwrite_run and os.path.exists(run_path):
+        #assert skip is False, 'skip must be False if overwrite_run is True'
+        shutil.rmtree(run_path)   
+        logger.info(f'Overwriting existing batch_run: {os.path.basename(run_path)}')      
+else:
+    run_path = unique_run_path
+
+## Create a directory to save the batch_run files
+if not os.path.exists(run_path):
+    os.makedirs(run_path)
+
+'''
+Generate Config
+'''
+#these updated params are based on the results from the following path:
+if selected_cand_cfg is not None: 
+    #selected_cand_cfg = 'batch_run_files/evol_params/evol_params_2021-07-07_16-00-00.json'
+    with open(selected_cand_cfg, 'r') as f:
+        initCfg = json.load(f)
+else: initCfg = None
+
+##Specify initFile, cfgFile and netParamsFile paths
+initFile = f'{script_path}/init.py'
+cfgFile = f'{script_path}/cfg.py'
+netParamsFile = f'{script_path}/netParams.py'
+
+assert os.path.exists(initFile), f'initFile does not exist: {initFile}'
+assert os.path.exists(cfgFile), f'cfgFile does not exist: {cfgFile}'
+assert os.path.exists(netParamsFile), f'netParamsFile does not exist: {netParamsFile}'
+
+##Create a dictionary with the given variables and their values
+batch_config_options = {
+    "run_path": run_path,
+    'batchLabel': os.path.basename(run_path),
+    "method": method,
+    "core_num": core_num,
+    "nodes": num_nodes,
+    "script": initFile,
+    #"pop_per_core": 1,
+    #"duration_seconds": 5,
+    "pop_size": pop_size,
+    "max_generations": 2000,
+    "time_sleep": 10, #seconds
+    "maxiter_wait": 6*40, #6itersx10sec/itersx30sec/min = 30 minutes per gen?
+    "skip": skip,
+    "num_elites": num_elites,
+    "cfgFile": cfgFile,
+    "netParamsFile": netParamsFile,
+    "initCfg": initCfg,
+}
+
+# # Write the dictionary to a JSON file
+# with open("batch_config_options.json", "w") as f:
+#     json.dump(batch_config_options, f)
