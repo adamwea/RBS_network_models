6025cded27c7a1dfa6dd0ec94b50facdbf142427
Adam Weiner <adammwea@login24.chn.perlmutter.nersc.gov>
2024-04-25 12:03:28 -0700

    moving data from last night's runs

---
+++ NERSC/job_inputs/2024-04-25_12-00-09_sbatch_jobscript_25AprRunWhileWork_2x128.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=25AprRunWhileWork_2x128
+#SBATCH -A m2043
+#SBATCH -t 03:00:00
+#SBATCH --nodes=2
+#SBATCH --output=NERSC/output/job_outputs/job_output_%j_25AprRunWhileWork_2x128.txt
+#SBATCH --error=NERSC/output/job_outputs/job_error_%j_25AprRunWhileWork_2x128.txt
+#SBATCH --mail-user=amwe@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -q regular
+#SBATCH -C cpu
+#SBATCH --exclusive
+    
+module load conda
+conda activate 2DSims
+module load openmpi
+mkdir -p NERSC/output/job_outputs
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+cd NERSC
+mpiexec --use-hwthread-cpus -np 256 -bind-to hwthread nrniv -mpi batchRun.py 25AprRunWhileWork_2x128 5
+    
