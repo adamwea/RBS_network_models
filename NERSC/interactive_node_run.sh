9d9cce4a0bc0b5a041aaa018a4c2363b6dacca39
Adam Weiner <adammwea@login16.chn.perlmutter.nersc.gov>
2024-04-24 22:23:24 -0700

    syncing

---
+++ NERSC/interactive_node_run.sh
@@ -0,0 +1,15 @@
+#module load python/3.9
+# export OMP_PROC_BIND=spread
+# export KMP_AFFINITY=verbose
+# export FI_LOG_LEVEL=debug
+
+module load conda
+conda activate 2DSims
+module load openmpi
+
+mkdir -p NERSC/output/job_outputs
+
+# Execute the application and capture all output
+#    mpiexec_command = f"mpiexec {mpiexec_flags} --use-hwthread-cpus -np {USER_total_cores} nrniv -mpi NERSC/batchRun.py {USER_JobName} {USER_seconds}"
+cd NERSC
+mpiexec --use-hwthread-cpus -np 256 nrniv -mpi batchRun.py scaling_up 30
