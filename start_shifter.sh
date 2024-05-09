2b46750ed62999dfbb63b7d032b0a89935f2f81a
Adam Weiner <adammwea@login36.chn.perlmutter.nersc.gov>
2024-05-09 11:10:07 -0700

    sync

--- start_shifter.sh
+++ start_shifter.sh
@@ -1,4 +1,7 @@
 echo "Starting the Docker container...enter 'exit' to close when done."
-shifterimg pull adammwea/netpyneshifter:1.0 - run this before running bash script
-shifter --image=adammwea/netpyneshifter:1.0 /bin/bash
+shifterimg pull kpkaur28/neuron:v3 - run this before running bash script
+shifter --image=kpkaur28/neuron:v3 /bin/bash
+# shifterimg pull adammwea/netpyneshifter:v2 - run this before running bash script
+# shifter --image=adammwea/netpyneshifter:v2 /bin/bash
 #cat /etc/lsb-release
+#kpkaur28/neuron:v3
--- test_shifter_interactive.sh
+++ test_shifter_interactive.sh
@@ -1,5 +1,6 @@
 #To run this, run these lines:
-#salloc --nodes=1 -C cpu -q interactive -t 04:00:00 --exclusive --image=adammwea/netpyneshifter:1.0
+#salloc --nodes=1 -C cpu -q interactive -t 04:00:00 --exclusive --image=adammwea/netpyneshifter:v2
+#salloc --nodes=1 -C cpu -q interactive -t 04:00:00 --exclusive --image=kpkaur28/neuron:v3
 #bash test_shifter_interactive.sh
 #SBATCH 
     
@@ -14,23 +15,23 @@ np=$((nodes*cores_per_node)) # 128 physical cores on the laptop
 ### Uncomment for testing MPI
 # Start the Docker container and get its ID
 #shifter mpiexec -np ${np} python3 testmpi.py #test mpi
-srun -n 4 -c 4 shifter --image=adammwea/netpyneshifter:1.0 python3 testmpi.py
+#srun --mpi=pmi2 -n 4 -c 1 shifter --image=adammwea/netpyneshifter:1.0 python3 testmpi.py
 
-# # Run commands inside the Docker container
-# echo "Starting the Docker container"
-# container_id=$(docker run -it -d -v $(pwd):/app test_image /bin/bash)
-# container_run_path=$(docker exec $container_id bash -c "python3 NERSC/USER_init_new_batch.py ${Batch_Run_Label}")
-# echo "Container path: ${container_run_path}"
-# full_sh_path_container=$(docker exec $container_id bash -c "realpath $0")
-# echo "Batch script path in container: ${full_sh_path_container}"
-# script_name=$(basename $0) #get the name of this batch script file
-# docker exec $container_id bash -c "cp ${full_sh_path_container} ${container_run_path}/${script_name}" #save copy of this batch_script in the run_path
-# echo "Running batch script inside the Docker container..."
-# docker exec $container_id bash -c "\
-#     cd NERSC &&\
-#     mpiexec -np ${np}\
-#     nrniv -mpi batchRun.py -rp ${container_run_path} -d ${Duration_Seconds}\
-#     > ${container_run_path}/mpi_output.txt 2> ${container_run_path}/mpi_error.txt"
-# echo "Batch script finished running inside the Docker container"
-# echo "Stopping the Docker container"
-# docker stop $container_id # Stop the Docker container when you're done
+# Run commands inside the Docker container
+echo "Starting the Docker container"
+container_id=$(docker run -it -d -v $(pwd):/app test_image /bin/bash)
+container_run_path=$(docker exec $container_id bash -c "python3 NERSC/USER_init_new_batch.py ${Batch_Run_Label}")
+echo "Container path: ${container_run_path}"
+full_sh_path_container=$(docker exec $container_id bash -c "realpath $0")
+echo "Batch script path in container: ${full_sh_path_container}"
+script_name=$(basename $0) #get the name of this batch script file
+docker exec $container_id bash -c "cp ${full_sh_path_container} ${container_run_path}/${script_name}" #save copy of this batch_script in the run_path
+echo "Running batch script inside the Docker container..."
+docker exec $container_id bash -c "\
+    cd NERSC &&\
+    mpiexec -np ${np}\
+    nrniv -mpi batchRun.py -rp ${container_run_path} -d ${Duration_Seconds}\
+    > ${container_run_path}/mpi_output.txt 2> ${container_run_path}/mpi_error.txt"
+echo "Batch script finished running inside the Docker container"
+echo "Stopping the Docker container"
+docker stop $container_id # Stop the Docker container when you're done
