1c704859aeae26d8385f828dd4e83d0d659753f0
Adam Weiner <adammwea@login12.chn.perlmutter.nersc.gov>
2024-05-22 12:51:30 -0700

    auto repo sync

--- NERSC/cfg.py
+++ NERSC/cfg.py
@@ -2,11 +2,45 @@ from netpyne import specs
 
 cfg = specs.SimConfig()
 
-cfg.networkType = 'pre13Apr24' #Network used for grant proposal in 01Apr24
+#cfg.networkType = 'pre13Apr24' #Network used for grant proposal in 01Apr24
+cfg.networkType = '22May24' #Network used for grant proposal in June 2024
 
 # --------------------------------------------------------
-# awa network
+# awa networks
 # --------------------------------------------------------
+if cfg.networkType == '22May24':
+	##
+	#cache_efficient - Use CVode cache_efficient option to optimize load when running on many cores (default: False)
+	cfg.cache_efficient = True
+
+	#seconds = cfg.duration_seconds
+	#cfg.duration = seconds*1e3           # Duration of the simulation, in ms
+	print('im here first')
+	cfg.dt = 0.025                # Internal integration timestep to use
+	cfg.verbose = False            # Show detailed messages
+	cfg.recordStep = 0.1             # Step size in ms to save data (eg. V traces, LFP, etc)
+	cfg.saveDataInclude = [
+		'simData', 
+		'simConfig', 
+		'netParams', 
+		'net'
+  		]
+	cfg.saveJson = True
+	cfg.printPopAvgRates = [100, cfg.duration]
+
+	#http://doc.netpyne.org/user_documentation.html#simconfig-recordtraces
+	from USER_const_net_params import netParams
+	num_Ecells = netParams.popParams['E']['numCells']
+	num_Icells = netParams.popParams['I']['numCells']
+	import random
+
+	# Choose two random cells from each population
+	E_cells = random.sample(range(num_Ecells), min(2, num_Ecells))
+	I_cells = random.sample(range(num_Icells), min(2, num_Icells))
+
+	cfg.recordCells = [('E', E_cells), ('I', I_cells)]
+	print(cfg.recordCells)
+
 if cfg.networkType == 'pre13Apr24':
 	##
 	#cache_efficient - Use CVode cache_efficient option to optimize load when running on many cores (default: False)
