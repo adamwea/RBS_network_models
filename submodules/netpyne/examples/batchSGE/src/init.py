cd09538429ececf8c3a24b35419325d9955b6dfa
adamwea <amwe@ucdavis.edu>
2024-09-30 13:12:42 -0700

    fixing submods

--- submodules/netpyne/examples/batchSGE/src/init.py
+++
@@ -1,7 +0,0 @@
-from netpyne import sim
-
-# read cfg and netParams from command line arguments if available; otherwise use default
-simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='src/cfg.py', netParamsDefault='src/params.py')
-
-# Create network and run simulation
-sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
--- submodules/netpyne/examples/cellDiversity/src/cfg.py
+++
@@ -1,4 +0,0 @@
-from netpyne import specs
-
-# Simulation options
-cfg = specs.SimConfig()        # object of class SimConfig to store simulation configuration
